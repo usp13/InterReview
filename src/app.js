7917d7e103bf547c545b376396267fb7cb2ddc2b
Utsav Panchal <utsavpanchal1304@gmail.com>
2026-06-18 01:19:23 +0530

    Cokkie parser added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require ( 'express')
+const cookieparser = require('cookie-parser')
+
+
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieparser())
 
 const authrouter= require('./routes/auth.routes')
 
