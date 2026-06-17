7482193b96c7bd228c6ec0d594a8441a15ba249f
Utsav Panchal <utsavpanchal1304@gmail.com>
2026-06-18 01:18:24 +0530

    added Blacklist Model

---
+++ src/models/blacklist.model.js
@@ -0,0 +1,15 @@
+const { Timestamp } = require('mongodb')
+const mongoose = require( 'mongoose' )
+
+const blacklisttokenschema = new mongoose.Schema({
+
+    token:{
+        type: String,
+        required: [ true , "Token is required to be add in blacklist"]
+    },
+   
+ }, {
+        timestamps: true 
+})
+
+const tokenblacklistmodel = mongoose.model("blacklisttoken" , blacklisttokenschema)
